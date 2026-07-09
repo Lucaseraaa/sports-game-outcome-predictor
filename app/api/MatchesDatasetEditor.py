536f1162e8f5b4a0748cf7d11c57c36b3a1ae4be
Luca Serafini <l.serafini@consorziometis.net>
2026-07-09 19:53:00 +0200

    aggiunta del metodo per il calcolo del PointToMatchRatio

--- app/api/MatchesDatasetEditor.py
+++ app/api/MatchesDatasetEditor.py
@@ -138,6 +138,52 @@ class MatchesDatasetEditor:
         return total_goals / total_shots_on_target
 
 
+    def __get_point_to_match_ratio(self, team: str, match_date: str, day: int) -> float:
+        """
+        Metodo utilizzato per calcolare il rapporto tra vittorie e partite disputate 
+        nelle ultime partite (max 5) della stagione in corso.
+
+        Args:
+            team: team di riferimento
+            match_date: data della partita in formato YYYY-MM-DD
+            day: giornata corrente di campionato
+
+        Returns:
+            Rapporto Vittorie / Partite disputate (float). 
+            Ritorna 0.0 in caso di prima giornata o se non ci sono partite precedenti.
+        """
+        # Caso limite
+        if day == 1:
+            return 0.0
+
+        year = int(match_date[:4])
+        month = int(match_date[5:7])
+        
+        season_start_year = year if month > 7 else year - 1
+        season_start = f"{season_start_year}-08-01"
+
+        
+        df = self.__dataset.reset_index()
+        is_playing = (df["HomeTeam"] == team) | (df["AwayTeam"] == team)
+        is_current_season_past = (df["Date"] >= season_start) & (df["Date"] < match_date)
+        
+
+        past_matches = df[is_playing & is_current_season_past].sort_values(by="Date", ascending=False).head(5)
+        total_matches = len(past_matches)
+
+        if total_matches == 0:
+            return 0.0
+
+        wins = 0
+        for _, match in past_matches.iterrows():
+            won_home = (match["HomeTeam"] == team) and (match["FTR"] == "H")
+            won_away = (match["AwayTeam"] == team) and (match["FTR"] == "A")
+
+            if won_home or won_away:
+                wins += 1
+
+        return wins / total_matches
+
     def add_in_dataset(self, day: int, statistics: Statistics) -> bool:
         """
         Metodo che permette di inserire un record (data + squadra casa + squadra trasferta) nel dataset
@@ -177,7 +223,11 @@ class MatchesDatasetEditor:
         baseline.append(self.__get_goal_on_shot_ratio(statistics.homeTeam.name, statistics.matchDate, day))
         baseline.append(self.__get_goal_on_shot_ratio(statistics.awayTeam.name, statistics.matchDate, day))
 
+        # Aggiunta del PointToMatchRatio
+        baseline.append(self.__get_point_to_match_ratio(statistics.homeTeam.name, statistics.matchDate, day))
+        baseline.append(self.__get_point_to_match_ratio(statistics.awayTeam.name, statistics.matchDate, day))
+
         print(baseline)
         
-        return True # Assumendo che tu voglia ritornare True al termine dell'operazione
+        return True 
         
