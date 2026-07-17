6766694c52b652ba4fee71bb29320ad5a3267fdf
lucaserafini <78957312+Lucaseraaa@users.noreply.github.com>
2026-07-17 17:48:42 +0200

    Revert "New/web api"

--- app/constants.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-# API values
-API_BASE_URL: str = os.getenv("API_BASE_URL", "https://localhost:8000/")
-API_TOKEN: str = os.getenv("API_TOKEN", "default")
-LEAGUE_ID: int = int(os.getenv("LEAGUE_ID", "0")) # Id della lega 'Serie A'
--- app/models/PlayerStatistics.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class PlayerStatistics(BaseModel):
-
-    homePlayersValue: float
-    awayPlayersValue: float
